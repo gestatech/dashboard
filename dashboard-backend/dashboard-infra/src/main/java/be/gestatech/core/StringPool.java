8a5ce1744ab3f425ffc27a97091f80665bedef1e
Gestatech <fariala.amuri@gestatech.be>
2017-06-17 20:26:43 +0200

    [DASHBOARD] Initial backend module setup

---
+++ dashboard-backend/dashboard-infra/src/main/java/be/gestatech/core/StringPool.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.gestatech.core;
+
+/**
+ *
+ * @author gestatech
+ */
+public class StringPool {
+
+    public static final String EMPTY = "";
+    public static final String SPACE = " ";
+    public static final String DASH = "-";
+    public static final String DOT = ".";
+    public static final String COMMA = ",";
+    public static final String NUMBER_SIGN = "#";
+}
