b155c3da539263eb5fce54ced9d7bd9944ca47d4
gestatech <fariala.amuri@gestatech.be>
2017-06-18 18:27:14 +0200

    [DASHBOARD] Add utilities

---
+++ dashboard-backend/dashboard-infra/src/main/java/be/gestatech/infra/util/ClassUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.gestatech.infra.util;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+/**
+ *
+ * @author gestatech
+ */
+public class ClassUtil {
+
+    public static Method getMethodIfAvailable(Class<?> clazz, String methodName, Class<?>... paramTypes) {
+        if (Objects.isNull(clazz)) {
+            return null;
+        }
+        try {
+            return clazz.getDeclaredMethod(methodName, paramTypes);
+        } catch (NoSuchMethodException e) {
+            // try superclass
+            return getMethodIfAvailable(clazz.getSuperclass(), methodName, paramTypes);
+        }
+    }
+}
--- dashboard-backend/dashboard-infra/src/main/java/be/gestatech/infra/util/StringUtils.java
+++ dashboard-backend/dashboard-infra/src/main/java/be/gestatech/infra/util/StringUtils.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package be.gestatech.core;
+package be.gestatech.infra.util;
+
+import java.util.Objects;
 
 /**
  *
  * @author gestatech
  */
-public class StringPool {
+public class StringUtils {
 
     public static final String EMPTY = "";
     public static final String SPACE = " ";
@@ -17,4 +19,19 @@ public class StringPool {
     public static final String DOT = ".";
     public static final String COMMA = ",";
     public static final String NUMBER_SIGN = "#";
+    public static final String LF = "\n";
+    public static final String CR = "\r";
+
+    private StringUtils() {
+        throw new RuntimeException();
+    }
+
+    public static boolean isEmpty(final CharSequence cs) {
+        return Objects.isNull(cs) || cs.length() == 0;
+    }
+
+    public static boolean isNotEmpty(final CharSequence cs) {
+        return !isEmpty(cs);
+    }
+
 }
